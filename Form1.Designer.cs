89fd38fe676c8f7a3581a1e6e105bf69bd5f4955
itel <itel@CHRISTINE-DIANG>
2023-12-12 14:10:01 +0100

    go

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,24 +29,13 @@ namespace GESTION_ET_SUPERVISION_DU_RESTAURANT
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(242, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,8 +45,6 @@ namespace GESTION_ET_SUPERVISION_DU_RESTAURANT
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
